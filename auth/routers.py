b1e6689a9b10c3865cc187043a869bc8b8704b6f
AlenGozy <=>
2023-10-07 18:13:28 +0600

    Purge

--- auth/routers.py
+++
@@ -1,6 +0,0 @@
-from .viewsets import RegisterViewSet
-from rest_framework.routers import DefaultRouter
-
-
-def register_auth_router(router: DefaultRouter):
-    router.register(r'register', RegisterViewSet, basename='register')
--- pwmanager/settings.py
+++ pwmanager/settings.py
@@ -144,6 +144,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'auth.User'
+AUTH_USER_MODEL = 'core.User'
 
 
